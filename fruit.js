a2108eaabdb677db5690e5b11277d240706aaaac
Hyunwoo Kim <32376551+KevinError@users.noreply.github.com>
2020-07-13 12:17:01 -0700

    creates class and constructor
    remove "function" and replace with class and constructor

--- fruit.js
+++ fruit.js
@@ -1,16 +1,19 @@
-function Fruit() {
-    this.x;
-    this.y;
-  
-    this.pickLocation = function() {
-      this.x = (Math.floor(Math.random() *
-        columns - 1) + 1) * scale;
-      this.y = (Math.floor(Math.random() *
-        rows - 1) + 1) * scale;
+
+class Fruit {
+    constructor() {
+        this.x;
+        this.y;
+
+        this.pickLocation = function () {
+            this.x = (Math.floor(Math.random() *
+                columns - 1) + 1) * scale;
+            this.y = (Math.floor(Math.random() *
+                rows - 1) + 1) * scale;
+        };
+
+        this.draw = function () {
+            ctx.fillStyle = "#4cafab";
+            ctx.fillRect(this.x, this.y, scale, scale);
+        };
     }
-  
-    this.draw = function() {
-      ctx.fillStyle = "#4cafab";
-      ctx.fillRect(this.x, this.y, scale, scale)
-    }
-  }
+}
